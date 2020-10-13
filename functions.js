72414e0016ba0035b9330a50ee17b29dcbe89d55
dgsdev <dgspramos@gmail.com>
2020-10-13 19:43:03 -0300

    Projeto feito com HTML, CSS e JS

---
+++ functions.js
@@ -0,0 +1,33 @@
+function exibir_categoria(categoria){
+    let elementos = document.getElementsByClassName('box_produto');
+    for(var i=0; i<elementos.length; i++){
+        if(categoria == elementos[i].id)
+            elementos[i].style = 'display:inline-block';
+        else
+            elementos[i].style = 'display:none';
+    }
+}
+
+
+let exibir_todos = () => {
+    let elementos = document.getElementsByClassName('box_produto');
+    for(var i=0; i<elementos.length;i++){
+        elementos[i].style = 'display:inline-block';
+    }
+}
+
+let zoom = (imagem) => {
+    if(imagem.width == 120)
+    imagem.style = 'width: 240px'
+    else
+    imagem.style = 'width: 120px'
+}
+
+
+function upScaleMenu(x){
+    x.style="font-weight: bold; color: red;";
+}
+
+function downScaleMenu(x){
+    x.style="font-weight: none; color: #000;";
+}
